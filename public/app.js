5ad38f399f2df9eb9f361491a2d88cd395619bb6
Avi Bagla <avibagla@gmail.com>
2020-04-14 10:42:18 -0700

    added gif

---
+++ public/app.js
@@ -0,0 +1,16 @@
+$(function(){
+	$(".title-image").click(function(){
+		console.log("here")
+		//toggle between the title image and the gif
+		if($(".title-image").attr("src") === "public/img/transparent headshot@0.2x.png"){
+			//means that the title image is the normal one
+			$(".title-image").attr("src", "public/img/fries.gif")
+		}else{
+			//other one
+			$(".title-image").attr("src", "public/img/transparent headshot@0.2x.png")
+		}
+
+	});
+});
+
+
